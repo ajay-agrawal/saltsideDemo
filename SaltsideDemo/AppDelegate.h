94fcdd82d92b63d07e9548011a4dd1da7692061c
agrawalajay <ajay@hem.com>
2015-11-22 12:48:00 +0530

    Initial Commit

---
+++ SaltsideDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaltsideDemo
+//
+//  Created by Ajay Agarwal on 11/22/15.
+//  Copyright © 2015 Ajay Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
